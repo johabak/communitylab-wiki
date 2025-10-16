68bbce65eed95f0a84ad0ea2a22069dc0d226831
Johan <johan.bakken@soprasteria.com>
2025-10-16 09:28:22 +0200

    testing2

--- update_docs.sh
+++ update_docs.sh
@@ -7,10 +7,10 @@ git checkout main
 git pull --ff-only origin main
 
 # 2) Bygg MkDocs -> ./site (rens opp gamle filer)
-sudo docker run --rm \
+docker run --rm \
   -v "$(pwd)":/docs \
   --user "$(id -u)":"$(id -g)" \
   squidfunk/mkdocs-material \
-  mkdocs build --clean
+  build --clean
 
 echo "✅ Ferdig bygget. Nginx serverer oppdaterte filer fra ./site"
